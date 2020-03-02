74e83d55c66889649e95a36329545019a7482c83
yyh-1 <3470259804@qq.com>
2020-03-02 15:32:48 +0800

    疫情

---
+++ test.js
@@ -0,0 +1,350 @@
+var btnxj = document.getElementsByClassName("xj")[0];
+var btnxz = document.getElementsByClassName("xz")[0];
+var btnqh = document.getElementsByClassName("qh")[0];
+var btngs = document.getElementsByClassName("gs")[0];
+var btnsc = document.getElementsByClassName("sc")[0];
+var btnyn = document.getElementsByClassName("yn")[0];
+var btnsx = document.getElementsByClassName("sx")[0];
+var btncq = document.getElementsByClassName("cq")[0];
+var btngz = document.getElementsByClassName("gz")[0];
+var btngx = document.getElementsByClassName("gx")[0];
+var btngd = document.getElementsByClassName("gd")[0];
+var btnhn = document.getElementsByClassName("hn")[0];
+var btnhb = document.getElementsByClassName("hb")[0];
+var btnhnhn = document.getElementsByClassName("hnhn")[0];
+var btnsxsx = document.getElementsByClassName("sxsx")[0];
+var btnhlj = document.getElementsByClassName("hlj")[0];
+var btnjl = document.getElementsByClassName("jl")[0];
+var btnln = document.getElementsByClassName("ln")[0];
+var btnsd = document.getElementsByClassName("sd")[0];
+var btnjs = document.getElementsByClassName("js")[0];
+var btnzj = document.getElementsByClassName("zj")[0];
+var btnjx = document.getElementsByClassName("jx")[0];
+var btnhj = document.getElementsByClassName("hj")[0];
+var btnah = document.getElementsByClassName("ah")[0];
+var btntw = document.getElementsByClassName("tw")[0];
+
+
+btnxj.onclick = function(){
+    document.getElementById("data1").innerHTML = "40";
+    document.getElementById("data2").innerHTML = "76";
+    document.getElementById("data3").innerHTML = "34";
+    document.getElementById("data4").innerHTML = "2";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-4";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+4";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnxz.onclick = function(){
+    document.getElementById("data1").innerHTML = "0";
+    document.getElementById("data2").innerHTML = "1";
+    document.getElementById("data3").innerHTML = "1";
+    document.getElementById("data4").innerHTML = "0";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnqh.onclick = function(){
+    document.getElementById("data1").innerHTML = "0";
+    document.getElementById("data2").innerHTML = "18";
+    document.getElementById("data3").innerHTML = "18";
+    document.getElementById("data4").innerHTML = "0";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btngs.onclick = function(){
+    document.getElementById("data1").innerHTML = "8";
+    document.getElementById("data2").innerHTML = "91";
+    document.getElementById("data3").innerHTML = "82";
+    document.getElementById("data4").innerHTML = "2";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-1";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+1";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnsc.onclick = function(){
+    document.getElementById("data1").innerHTML = "211";
+    document.getElementById("data2").innerHTML = "534";
+    document.getElementById("data3").innerHTML = "320";
+    document.getElementById("data4").innerHTML = "3";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-15";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+3";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+18";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnyn.onclick = function(){
+    document.getElementById("data1").innerHTML = "22";
+    document.getElementById("data2").innerHTML = "174";
+    document.getElementById("data3").innerHTML = "150";
+    document.getElementById("data4").innerHTML = "2";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-15";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+15";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnsx.onclick = function(){
+    document.getElementById("data1").innerHTML = "49";
+    document.getElementById("data2").innerHTML = "245";
+    document.getElementById("data3").innerHTML = "195";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-8";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+8";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btncq.onclick = function(){
+    document.getElementById("data1").innerHTML = "186";
+    document.getElementById("data2").innerHTML = "576";
+    document.getElementById("data3").innerHTML = "384";
+    document.getElementById("data4").innerHTML = "6";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-12";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+12";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btngz.onclick = function(){
+    document.getElementById("data1").innerHTML = "32";
+    document.getElementById("data2").innerHTML = "146";
+    document.getElementById("data3").innerHTML = "112";
+    document.getElementById("data4").innerHTML = "2";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-8";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+8";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btngx.onclick = function(){
+    document.getElementById("data1").innerHTML = "77";
+    document.getElementById("data2").innerHTML = "252";
+    document.getElementById("data3").innerHTML = "173";
+    document.getElementById("data4").innerHTML = "2";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-21";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+21";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btngd.onclick = function(){
+    document.getElementById("data1").innerHTML = "442";
+    document.getElementById("data2").innerHTML = "1347";
+    document.getElementById("data3").innerHTML = "898";
+    document.getElementById("data4").innerHTML = "7";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-32";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+32";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnhn.onclick = function(){
+    document.getElementById("data1").innerHTML = "209";
+    document.getElementById("data2").innerHTML = "1017";
+    document.getElementById("data3").innerHTML = "804";
+    document.getElementById("data4").innerHTML = "4";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-13";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+1";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+14";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnhb.onclick = function(){
+    document.getElementById("data1").innerHTML = "39586";
+    document.getElementById("data2").innerHTML = "65596";
+    document.getElementById("data3").innerHTML = "23369";
+    document.getElementById("data4").innerHTML = "2641";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-1905";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+409";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+2288";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+26";
+}
+btnhnhn.onclick = function(){
+    document.getElementById("data1").innerHTML = "185";
+    document.getElementById("data2").innerHTML = "1272";
+    document.getElementById("data3").innerHTML = "1067";
+    document.getElementById("data4").innerHTML = "20";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-39";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+1";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+39";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+1";
+}
+btnsxsx.onclick = function(){
+    document.getElementById("data1").innerHTML = "27";
+    document.getElementById("data2").innerHTML = "133";
+    document.getElementById("data3").innerHTML = "106";
+    document.getElementById("data4").innerHTML = "0";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-6";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+6";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnhlj.onclick = function(){
+    document.getElementById("data1").innerHTML = "196";
+    document.getElementById("data2").innerHTML = "480";
+    document.getElementById("data3").innerHTML = "271";
+    document.getElementById("data4").innerHTML = "13";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-18";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+17";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+1";
+}
+btnjl.onclick = function(){
+    document.getElementById("data1").innerHTML = "25";
+    document.getElementById("data2").innerHTML = "93";
+    document.getElementById("data3").innerHTML = "67";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-2";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+2";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnln.onclick = function(){
+    document.getElementById("data1").innerHTML = "23";
+    document.getElementById("data2").innerHTML = "121";
+    document.getElementById("data3").innerHTML = "97";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-4";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+4";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnsd.onclick = function(){
+    document.getElementById("data1").innerHTML = "363";
+    document.getElementById("data2").innerHTML = "756";
+    document.getElementById("data3").innerHTML = "387";
+    document.getElementById("data4").innerHTML = "6";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-24";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+24";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnjs.onclick = function(){
+    document.getElementById("data1").innerHTML = "133";
+    document.getElementById("data2").innerHTML = "631";
+    document.getElementById("data3").innerHTML = "498";
+    document.getElementById("data4").innerHTML = "0";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-31";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+31";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnzj.onclick = function(){
+    document.getElementById("data1").innerHTML = "272";
+    document.getElementById("data2").innerHTML = "1205";
+    document.getElementById("data3").innerHTML = "932";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-67";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+67";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnjx.onclick = function(){
+    document.getElementById("data1").innerHTML = "179";
+    document.getElementById("data2").innerHTML = "934";
+    document.getElementById("data3").innerHTML = "754";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-35";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+35";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnhj.onclick = function(){
+    document.getElementById("data1").innerHTML = "67";
+    document.getElementById("data2").innerHTML = "296";
+    document.getElementById("data3").innerHTML = "228";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-18";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+2";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+20";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btnah.onclick = function(){
+    document.getElementById("data1").innerHTML = "197";
+    document.getElementById("data2").innerHTML = "989";
+    document.getElementById("data3").innerHTML = "786";
+    document.getElementById("data4").innerHTML = "6";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "昨日-37";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "昨日+0";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "昨日+37";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "昨日+0";
+}
+btntw.onclick = function(){
+    document.getElementById("data1").innerHTML = "26";
+    document.getElementById("data2").innerHTML = "32";
+    document.getElementById("data3").innerHTML = "5";
+    document.getElementById("data4").innerHTML = "1";
+    document.getElementsByClassName("yestoday")[0].innerHTML = "";
+    document.getElementsByClassName("yestoday")[1].innerHTML = "";
+    document.getElementsByClassName("yestoday")[2].innerHTML = "";
+    document.getElementsByClassName("yestoday")[3].innerHTML = "";
+}
+
+// document.getElementsByClassName("xjdata")[0].style.width = "100px"
+
+
+
+
+
+
+var newtrend = document.getElementsByClassName("newtrend")[0];
+var maytrend = document.getElementsByClassName("maytrend")[0];
+var sumtrend = document.getElementsByClassName("sumtrend")[0];
+var healtrend = document.getElementsByClassName("healtrend")[0];
+
+newtrend.onclick = function(){
+    document.getElementsByClassName("img")[0].style.backgroundImage = "url(img1.png)";
+    document.getElementsByClassName("newtrend")[0].style.backgroundColor = "#10aeb5";
+    document.getElementsByClassName("maytrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("sumtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("healtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("p1")[0].style.color = "#fff";
+    document.getElementsByClassName("p2")[0].style.color = "#fff";
+    document.getElementsByClassName("p1")[1].style.color = "#000";
+    document.getElementsByClassName("p2")[1].style.color = "#000";
+    document.getElementsByClassName("p1")[2].style.color = "#000";
+    document.getElementsByClassName("p2")[2].style.color = "#000";
+    document.getElementsByClassName("p1")[3].style.color = "#000";
+    document.getElementsByClassName("p2")[3].style.color = "#000";
+}
+maytrend.onclick = function(){
+    document.getElementsByClassName("img")[0].style.backgroundImage = "url(img2.png)";
+    document.getElementsByClassName("newtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("maytrend")[0].style.backgroundColor = "#10aeb5";
+    document.getElementsByClassName("sumtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("healtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("p1")[0].style.color = "#000";
+    document.getElementsByClassName("p2")[0].style.color = "#000";
+    document.getElementsByClassName("p1")[1].style.color = "#fff";
+    document.getElementsByClassName("p2")[1].style.color = "#fff";
+    document.getElementsByClassName("p1")[2].style.color = "#000";
+    document.getElementsByClassName("p2")[2].style.color = "#000";
+    document.getElementsByClassName("p1")[3].style.color = "#000";
+    document.getElementsByClassName("p2")[3].style.color = "#000";
+}
+sumtrend.onclick = function(){
+    document.getElementsByClassName("img")[0].style.backgroundImage = "url(img3.png)";
+    document.getElementsByClassName("newtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("maytrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("sumtrend")[0].style.backgroundColor = "#10aeb5";
+    document.getElementsByClassName("healtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("p1")[0].style.color = "#000";
+    document.getElementsByClassName("p2")[0].style.color = "#000";
+    document.getElementsByClassName("p1")[1].style.color = "#000";
+    document.getElementsByClassName("p2")[1].style.color = "#000";
+    document.getElementsByClassName("p1")[2].style.color = "#fff";
+    document.getElementsByClassName("p2")[2].style.color = "#fff";
+    document.getElementsByClassName("p1")[3].style.color = "#000";
+    document.getElementsByClassName("p2")[3].style.color = "#000";
+}
+healtrend.onclick = function(){
+    document.getElementsByClassName("img")[0].style.backgroundImage = "url(img4.png)";
+    document.getElementsByClassName("newtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("maytrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("sumtrend")[0].style.backgroundColor = "#aaa";
+    document.getElementsByClassName("healtrend")[0].style.backgroundColor = "#10aeb5";
+    document.getElementsByClassName("p1")[0].style.color = "#000";
+    document.getElementsByClassName("p2")[0].style.color = "#000";
+    document.getElementsByClassName("p1")[1].style.color = "#000";
+    document.getElementsByClassName("p2")[1].style.color = "#000";
+    document.getElementsByClassName("p1")[2].style.color = "#000";
+    document.getElementsByClassName("p2")[2].style.color = "#000";
+    document.getElementsByClassName("p1")[3].style.color = "#fff";
+    document.getElementsByClassName("p2")[3].style.color = "#fff";
+}
